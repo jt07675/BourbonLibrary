52c05f8fd4e75c42047371345d1ac221af5e8d40
jt07675 <jasontimbo@gmail.com>
2016-12-22 00:18:10 -0700

    Created .swift files for various view controllers. Continued building out storyboard.

---
+++ BourbonLibrary/SignInLandingViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignInLandingViewController.swift
+//  Bourbon Library
+//
+//  Created by Timbo Family on 12/21/16.
+//  Copyright © 2016 Jason Timbo. All rights reserved.
+//
+
+import UIKit
+
+class SignInLandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
